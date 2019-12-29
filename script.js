95c4d4febf3f74b3978e0d2af9b1cc47d353fdc1
Jack <58533031+jackherizsmith@users.noreply.github.com>
2019-12-29 17:41:52 +0545

    Autoplay button update
    Now it's changing colour when clicked!

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 window.onload = function() {
     let playing = true;
+    let slideNumber = 0;
 
     function updateImage() {
+        slideNumber = window.location.href.replace(/[\D]/g, '');
         if (playing) {
-            i++;
-            if (i>6){i=1}
-            window.location.href("https://jackherizsmith.github.io/#carousel-image-");
+            slideNumber++;
+            if (slideNumber>6){slideNumber=1}
+            window.location.href("https://jackherizsmith.github.io/#carousel-image-"+slideNumber);
         }
         window.location.href("https://jackherizsmith.github.io/#carousel")
     }
-
-    var autoplay = document.getElementById("autoplay");
     
-    function autoplay() {
+    document.getElementById("autoplay-button").onclick = function(){
+
+        document.getElementById("autoplay-button").style.backgroundColor = 'rgba(61,153,112, 0.7)';
         if (playing) {
             playing = false;
             document.getElementById("autoplay-button").style.backgroundColor = 'rgba(247,63,58, 0.7)';
@@ -21,7 +23,6 @@ window.onload = function() {
             document.getElementById("autoplay-button").style.backgroundColor = 'rgba(61,153,112, 0.7)';
         }
     };
-
 
     setInterval(updateImage, 1000);
 }
