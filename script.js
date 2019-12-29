b1e341d84517c62bcbe480a04f40f07eae04771a
Jack <58533031+jackherizsmith@users.noreply.github.com>
2019-12-29 21:30:06 +0545

    Image carousel finished
    Got the autoplay working, and adding key functionality was fairly straightforward. Added:
    spacebar = toggle autoplay
    left = go left
    right = go right
    1-6 = jump to image #

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let playing = false;
-let slideNumber = -1;
+let slideNumber = 0;
 let offset = "";
 
 function updateImage() {
@@ -13,28 +13,56 @@ function updateImage() {
 }
 
 function autoplay() {
-    if (playing) {
-        playing = false;
-        document.getElementById("autoplay-button").style.backgroundColor = 'rgba(247,63,58, 0.7)';
-    } else {
-        playing = true;
-        document.getElementById("autoplay-button").style.backgroundColor = 'rgba(61,153,112, 0.7)';
-    }
-};
+    playing = !playing;
+    document.getElementById("auto-on").style.backgroundColor = playing ? 
+    'rgba(61,153,112, 0.7)' : 
+    'rgba(247,63,58, 0.7)';
+}
 
 function setOffset(slideNumber){
-    offset = "" + (-720 * slideNumber) + "px";
+    offset = (-720 * slideNumber) + "px";
     document.getElementById("imageBox").style.left = offset;
 }
 
 function setSlideNumber(n){
-    if (slideNumber !== -1) {
-        document.getElementById(`carBut${slideNumber}`).style.backgroundColor = 'white';
-    }
-    document.getElementById(`carBut${n}`).style.backgroundColor = 'red';
-    console.log(slideNumber)
+    document.getElementById(`carBut${slideNumber}`).style.backgroundColor = '#166088';
+    document.getElementById(`carBut${n}`).style.backgroundColor = '#a1cdf4';
     slideNumber = n;
     setOffset(slideNumber);
 }
 
+document.onkeydown = function(event) {
+    if(event.keyCode == 37){
+        if (slideNumber == 0) {n = 5}
+        else (n = slideNumber - 1)
+        setSlideNumber(n);
+    }
+    else if(event.keyCode == 39){
+        if (slideNumber == 5) {n = 0}
+        else (n = slideNumber + 1)
+        setSlideNumber(n);
+    }
+    else if(event.keyCode == 32){
+        event.preventDefault();
+        autoplay();
+    }
+    else if(event.keyCode == 49){
+        setSlideNumber(0);
+    }
+    else if(event.keyCode == 50){
+        setSlideNumber(1);
+    }
+    else if(event.keyCode == 51){
+        setSlideNumber(2);
+    }
+    else if(event.keyCode == 52){
+        setSlideNumber(3);
+    }
+    else if(event.keyCode == 53){
+        setSlideNumber(4);
+    }
+    else if(event.keyCode == 54){
+        setSlideNumber(5);
+    }
+}
 setInterval(updateImage, 2000);
