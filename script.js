a05ac0a825311d24177063e9fa4a4aeea5c7c979
Jack <58533031+jackherizsmith@users.noreply.github.com>
2019-12-29 19:25:42 +0545

    Autoplay now working
    Got autoplay to work, as well as buttons to change colour on selection which was missing before. Now got to add left and right navigation and tidy up colour contrast in CSS

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-window.onload = function() {
-    let playing = true;
-    let slideNumber = 0;
+let playing = false;
+let slideNumber = -1;
+let offset = "";
 
-    function updateImage() {
-        slideNumber = window.location.href.replace(/[\D]/g, '');
-        if (playing) {
-            slideNumber++;
-            if (slideNumber>6){slideNumber=1}
-            window.location.href("https://jackherizsmith.github.io/#carousel-image-"+slideNumber);
-        }
-        window.location.href("https://jackherizsmith.github.io/#carousel")
-    }
-    
-    document.getElementById("autoplay-button").onclick = function(){
-
-        document.getElementById("autoplay-button").style.backgroundColor = 'rgba(61,153,112, 0.7)';
-        if (playing) {
-            playing = false;
-            document.getElementById("autoplay-button").style.backgroundColor = 'rgba(247,63,58, 0.7)';
+function updateImage() {
+    if (playing) {            
+        if (slideNumber == 5){
+            setSlideNumber(0);
         } else {
-            playing = true;
-            document.getElementById("autoplay-button").style.backgroundColor = 'rgba(61,153,112, 0.7)';
+            setSlideNumber(slideNumber + 1);
         }
-    };
-
-    setInterval(updateImage, 1000);
+    }
 }
+
+function autoplay() {
+    if (playing) {
+        playing = false;
+        document.getElementById("autoplay-button").style.backgroundColor = 'rgba(247,63,58, 0.7)';
+    } else {
+        playing = true;
+        document.getElementById("autoplay-button").style.backgroundColor = 'rgba(61,153,112, 0.7)';
+    }
+};
+
+function setOffset(slideNumber){
+    offset = "" + (-720 * slideNumber) + "px";
+    document.getElementById("imageBox").style.left = offset;
+}
+
+function setSlideNumber(n){
+    if (slideNumber !== -1) {
+        document.getElementById(`carBut${slideNumber}`).style.backgroundColor = 'white';
+    }
+    document.getElementById(`carBut${n}`).style.backgroundColor = 'red';
+    console.log(slideNumber)
+    slideNumber = n;
+    setOffset(slideNumber);
+}
+
+setInterval(updateImage, 2000);
